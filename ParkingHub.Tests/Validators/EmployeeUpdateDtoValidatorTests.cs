adc92dccdb5d004646e605516f4b5ed875fd7aa7
Joao <j.p.santos21@hotmail.com>
2025-11-11 23:24:57 +0000

    Added tests

---
+++ ParkingHub.Tests/Validators/EmployeeUpdateDtoValidatorTests.cs
@@ -0,0 +1,120 @@
+﻿using FluentValidation.TestHelper;
+using ParkingHub.Data.DTOs.Employee;
+using ParkingHub.Validators;
+
+namespace ParkingHub.Tests.Validators
+{
+    [TestFixture]
+    public class EmployeeUpdateDtoValidatorTests
+    {
+        private EmployeeUpdateDtoValidator _validator = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new EmployeeUpdateDtoValidator();
+        }
+
+        [Test]
+        public void Name_LessThan3Chars_ShouldFail()
+        {
+            var dto = new EmployeeUpdateDto { Name = "Al", LicensePlates = ["AA-11-AA"] };
+            var result = _validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Test]
+        public void Name_TooLong_ShouldFail()
+        {
+            var dto = new EmployeeUpdateDto
+            {
+                Name = new string('A', 51),
+                LicensePlates = ["AA-11-AA"]
+            };
+
+            var result = _validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Test]
+        public void Name_WithinValidRange_ShouldPass()
+        {
+            var dto = new EmployeeUpdateDto
+            {
+                Name = "Alice",
+                LicensePlates = ["AA-11-AA"]
+            };
+
+            var result = _validator.TestValidate(dto);
+            result.ShouldNotHaveValidationErrorFor(x => x.Name);
+        }
+
+        [Test]
+        public void LicensePlates_Null_ShouldFail()
+        {
+            var dto = new EmployeeUpdateDto { Name = "Alice", LicensePlates = null };
+            var result = _validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor(x => x.LicensePlates);
+        }
+
+        [Test]
+        public void LicensePlates_Empty_ShouldFail()
+        {
+            var dto = new EmployeeUpdateDto { Name = "Alice", LicensePlates = [] };
+            var result = _validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor(x => x.LicensePlates);
+        }
+
+        [Test]
+        public void LicensePlates_TooMany_ShouldFail()
+        {
+            var dto = new EmployeeUpdateDto
+            {
+                Name = "Alice",
+                LicensePlates = ["AA-11-AA", "BB-22-BB", "CC-33-CC", "DD-44-DD"]
+            };
+
+            var result = _validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor(x => x.LicensePlates);
+        }
+
+        [Test]
+        public void LicensePlates_InvalidFormat_ShouldFail()
+        {
+            var dto = new EmployeeUpdateDto
+            {
+                Name = "Alice",
+                LicensePlates = ["INVALID"]
+            };
+
+            var result = _validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor("LicensePlates[0]");
+        }
+
+        [Test]
+        public void LicensePlates_ValidFormat_ShouldPass()
+        {
+            var dto = new EmployeeUpdateDto
+            {
+                Name = "Alice",
+                LicensePlates = ["AA-11-AA", "BB-22-BB"]
+            };
+
+            var result = _validator.TestValidate(dto);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Test]
+        public void LicensePlates_ExactlyThree_ShouldPass()
+        {
+            var dto = new EmployeeUpdateDto
+            {
+                Name = "Alice",
+                LicensePlates = ["AA-11-AA", "BB-22-BB", "CC-33-CC"]
+            };
+
+            var result = _validator.TestValidate(dto);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
--- ParkingHub/Validators/EmployeeUpdateDtoValidator.cs
+++ ParkingHub/Validators/EmployeeUpdateDtoValidator.cs
@@ -8,19 +8,34 @@ namespace ParkingHub.Validators
         public EmployeeUpdateDtoValidator()
         {
             RuleFor(x => x.Name)
-                .NotEmpty()
-                .MaximumLength(50);
+                .NotEmpty().WithMessage("Name is required.")
+                .MinimumLength(3).WithMessage("Name must be at least 3 characters.")
+                .MaximumLength(50).WithMessage("Name must be at most 50 characters.");
 
             RuleFor(x => x.LicensePlates)
-                .NotNull()
-                .Must(lp => lp.Count <= 3)
-                .WithMessage("An employee can have at most 3 license plates.");
+                .NotNull().WithMessage("License plates list cannot be null.");
 
-            RuleForEach(x => x.LicensePlates)
-                .NotEmpty()
-                .MaximumLength(8)
-                .Matches(@"^[A-Z0-9]{2}-[A-Z0-9]{2}-[A-Z0-9]{2}$")
-                .WithMessage("Each plate must be in format XX-XX-XX with letters or numbers.");
+            When(x => x.LicensePlates != null, () =>
+            {
+                RuleFor(x => x.LicensePlates)
+                    .NotEmpty()
+                    .WithMessage("At least one license plate is required.");
+
+                RuleFor(x => x.LicensePlates)
+                    .Must(plates => plates.Distinct().Count() == plates.Count)
+                    .WithMessage("License plates must be unique.");
+
+                RuleFor(x => x.LicensePlates)
+                    .Must(plates => plates.Count <= 3)
+                    .WithMessage("An employee can have at most 3 license plates.");
+
+                RuleForEach(x => x.LicensePlates)
+                    .Cascade(CascadeMode.Stop)
+                    .NotEmpty().WithMessage("Plate number cannot be empty.")
+                    .MaximumLength(8).WithMessage("Plate number must be at most 8 characters.")
+                    .Matches(@"^[A-Z0-9]{2}-[A-Z0-9]{2}-[A-Z0-9]{2}$")
+                    .WithMessage("Each plate must be in format XX-XX-XX with letters or numbers.");
+            });
         }
     }
 }
