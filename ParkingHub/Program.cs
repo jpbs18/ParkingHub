ed9686080fe2acb01a7ba4c8c717317aa8529c70
Joao <j.p.santos21@hotmail.com>
2025-11-12 21:12:05 +0000

    Testing pipeline

--- ParkingHub/Program.cs
+++ ParkingHub/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAppServices(builder.Configuration);
 
 var app = builder.Build();
+
 app.ApplyMigrationsAndSeed();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.MapControllers();
